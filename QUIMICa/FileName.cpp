b02df8e35c560ab1174137d7eaaa7e5cb4e22052
MartimFilipe <156333450+MartimFilipe@users.noreply.github.com>
2024-02-27 16:05:33 +0000

    Add files via upload

---
+++ QUIMICa/FileName.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>​
+#include <string>​
+#include <locale.h>
+#include <math.h>
+string n; 
+
+using namespace std;
+int main()
+{
+	do 
+	{
+		cin >> n;
+	} while (1 <= n && n < 1000);
+
+	if (isupper(n))
+	{
+		isdigit(n);
+		
+	}
+
+		return 0;
+}
