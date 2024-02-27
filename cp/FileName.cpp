3833f6ce0f29186162577f1103fbbad573b49c55
MartimFilipe <156333450+MartimFilipe@users.noreply.github.com>
2024-02-27 16:10:50 +0000

    Add files via upload

---
+++ cp/FileName.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>​
+#include <string>​
+#include <locale.h>
+#include <math.h>
+float p, d, g ,i, r, f ; // p= preço ,d = dias , g= grupo , r = DECIMAS, f = final 
+using namespace std;
+int main()
+{
+	do {
+		cin >> p;
+		cin >> d;
+		cin >> g;
+		
+	} while (100 <= p && p < 1000);
+	p = p / 100;
+	p = p - p * 0.25;
+	r = p - trunc(p);
+	if (r > 0.5)
+	{
+		f = trunc(p) + 1;
+	}
+	else
+	{
+		f = trunc(p) + 0.5;
+	}
+	p = f;
+
+
+	if (d > 4 && d < 9)
+	{
+		p = p - p * 0.4;
+		r = p - trunc(p);
+		if (r > 0.5)
+		{
+			f = trunc(p) + 1;
+		}
+		else
+		{
+			f = trunc(p) + 0.5;
+		}
+		p = f;
+	}
+	else if (d > 8)
+	{
+		p = p - p * 0.65;
+		r = p -  trunc(p);
+		if (r > 0.5)
+		{
+			f = trunc(p) + 1;
+		}
+		else
+		{
+			f = trunc(p) + 0.5;
+		}
+		p = f;
+	}
+	if (g > 3)
+	{
+		p = p - p * 0.65;
+		r = p - trunc(p);
+		if (r > 0.5)
+		{
+			f = trunc(p) + 1;
+		}
+		else
+		{
+			f = trunc(p) + 0.5;
+		}
+		p = f;
+	}
+	p = f * 100;
+	cout << p;
+
+
+
+
+	return 0;
+}
